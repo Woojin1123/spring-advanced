3a0d4b7e70812c5230d9fa34d0dc477ac4daee3f
Woojin1123 <boom175839@gmail.com>
2024-09-11 17:32:23 +0900

    1-1 Early Return

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -27,13 +27,20 @@ public class AuthService {
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
 
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
-
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
-
+        /*
+        * 1-1 Early Return
+        * if (userRepository.existsByEmail(signupRequest.getEmail())) {
+               throw new InvalidRequestException("이미 존재하는 이메일입니다.");
+          }
+          * 해당 에러가 발생할 경우 encode가 실행되지 않게 개선
+          * */
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
+
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+
+        UserRole userRole = UserRole.of(signupRequest.getUserRole());
 
         User newUser = new User(
                 signupRequest.getEmail(),
